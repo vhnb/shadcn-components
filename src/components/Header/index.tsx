a25f061b59306ed17231e32bc18efc51374c5e85
victor <victorredin122@gmail.com>
2025-06-09 23:18:08 -0300

    #

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -6,10 +6,6 @@ import {
     DropdownMenuSeparator,
     DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
-import { Toaster } from "@/components/ui/sonner"
-import { Button } from "@/components/ui/button"
-import { toast } from "sonner"
-import { FaUser } from "react-icons/fa";
 import {
     AlertDialog,
     AlertDialogAction,
@@ -21,8 +17,14 @@ import {
     AlertDialogTitle,
     AlertDialogTrigger,
 } from "@/components/ui/alert-dialog"
+import { Toaster } from "@/components/ui/sonner"
+import { Button } from "@/components/ui/button"
+import { toast } from "sonner"
+import { FaUser } from "react-icons/fa";
+import { useSession } from "next-auth/react"
 
 export default function Header() {
+    const { data: session } = useSession()
 
     function handleEvent() {
         window.location.href = '/auth/login'
@@ -38,19 +40,22 @@ export default function Header() {
                 <div className="flex items-center justify-between max-w-[1000px] w-[90%]">
                     <h1 onClick={handleEventToHome} className="cursor-pointer font-bold text-[20px]">Your<span className="ml-1 bg-zinc-800 text-[15px] py-1 px-2 rounded-[6px] text-white">Notes</span></h1>
                     <div className="flex">
-                        <DropdownMenu>
-                            <DropdownMenuTrigger>
-                                <Button className="mr-1" variant='outline'><FaUser color="#b4b4b4" /> Minha conta</Button>
-                            </DropdownMenuTrigger>
-                            <DropdownMenuContent>
-                                <DropdownMenuLabel>Opções</DropdownMenuLabel>
-                                <DropdownMenuSeparator />
-                                <DropdownMenuItem onClick={() => window.location.href = 'dashboard'}>Dashboard</DropdownMenuItem>
-                                <DropdownMenuSeparator />
-                                <DropdownMenuItem>Sair</DropdownMenuItem>
-                            </DropdownMenuContent>
-                        </DropdownMenu>
-                        <Button onClick={handleEvent} variant="outline">Entrar</Button>
+                        {session ? (
+                            <DropdownMenu>
+                                <DropdownMenuTrigger>
+                                    <Button className="mr-1" variant='outline'><FaUser color="#b4b4b4" /> Minha conta</Button>
+                                </DropdownMenuTrigger>
+                                <DropdownMenuContent>
+                                    <DropdownMenuLabel>Opções</DropdownMenuLabel>
+                                    <DropdownMenuSeparator />
+                                    <DropdownMenuItem onClick={() => window.location.href = 'dashboard'}>Dashboard</DropdownMenuItem>
+                                    <DropdownMenuSeparator />
+                                    <DropdownMenuItem>Sair</DropdownMenuItem>
+                                </DropdownMenuContent>
+                            </DropdownMenu>
+                        ) : (
+                            <Button onClick={handleEvent} variant="outline">Entrar</Button>
+                        )}
                     </div>
                 </div>
             </header>
